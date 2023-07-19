7cc9014f52b485f29464fb19d136e9d7a1c224db
Monika <412.kri@gmail.com>
2023-07-19 23:08:39 +0300

    data structure

--- src/components/main/Data.jsx
+++ src/components/main/Data.jsx
@@ -2,7 +2,51 @@ import style from './Data.module.css';
 
 function Data() {
     return  (
-        <div>data</div>
+        <div className={style.data}>
+           <h4> Today's data</h4>
+           <div className={style.column}>
+                <div className={style.columncolor1}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+                <div className={style.columncolor2}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+                <div className={style.columncolor3}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+                <div className={style.columncolor4}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+                <div className={style.columncolor5}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+                <div className={style.columncolor5}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+                <div className={style.columncolor5}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+                <div className={style.columncolor5}>
+                    <p>text</p>
+                    <p>number</p>
+                    <p>text</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
